fe87c1bb844a8b3d67e071254c2259bc9354da17
dhavap <50911475+dhavap@users.noreply.github.com>
2020-12-10 18:24:07 -0800

    merge themes

---
+++ portfolio 2/js/myscripts.js
@@ -0,0 +1,16 @@
+//DYNAMIC YEAR
+var footeryear = document.getElementById("year")
+footeryear.innerHTML = new Date().getFullYear()
+
+
+function sendEmail() {
+    Email.send({
+        SecureToken: "6417b022-d0de-4dc2-b10b-40d09244b585",
+        To: 'dhavalakshmi.p@gmail.com',
+        From: "",
+        Subject: "Email from portfolio site",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+}
